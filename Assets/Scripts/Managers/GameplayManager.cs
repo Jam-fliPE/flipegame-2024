71509df1f460f7059acd77a1a560839644a5b95e
Henrique <henrique.dsc@gmail.com>
2024-07-08 00:41:23 -0300

    Added ai controller. Added gameplay manager.

---
+++ Assets/Scripts/Managers/GameplayManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _player;
+
+    private static GameplayManager _instance = null;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public static GameplayManager GetInstance()
+    {
+        return _instance;
+    }
+
+    public GameObject GetPlayer()
+    {
+        return _player;
+    }
+}
