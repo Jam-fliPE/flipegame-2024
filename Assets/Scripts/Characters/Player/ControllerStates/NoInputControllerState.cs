430ee51b5a368525b8afb59d0a4716d74d2069f0
Henrique <henrique.dsc@gmail.com>
2024-07-13 19:59:06 -0300

    Fixed player moving after take a hit.

--- Assets/Scripts/Characters/Player/ControllerStates/NoInputControllerState.cs
+++ Assets/Scripts/Characters/Player/ControllerStates/NoInputControllerState.cs
@@ -1,4 +1,10 @@
 public class NoInputState : AControllerState
 {
+    private CharacterMovement _movement;
 
+    public override void OnEnter(PlayerController controller)
+    {
+        _movement ??= controller.GetComponent<CharacterMovement>();
+        _movement.SetDirection(UnityEngine.Vector3.zero);
+    }
 }
