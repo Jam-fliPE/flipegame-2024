f717e231f4595c108b7f098d7e8e3b8acf5e7e81
Henrique <henrique.dsc@gmail.com>
2024-08-01 23:31:16 -0300

    Fixed scene rocks.

--- Assets/Scripts/WorldCycle/RandomTransformItems.cs
+++ Assets/Scripts/WorldCycle/RandomTransformItems.cs
@@ -1,13 +1,16 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomTransformItems : MonoBehaviour
 {
     [SerializeField]
-    private Transform[] _items;
+    private Transform[] _itemsToSwitchLocations;
+    [SerializeField]
+    private Transform[] _itemsToRotate;
     [SerializeField]
     private float _positionRange = 2.0f;
 
-    private Vector3[] _initialPositions;
+    private float[] _initialPositions;
     private Transform _transform;
 
     private void Start()
@@ -18,20 +21,35 @@ public class RandomTransformItems : MonoBehaviour
     [ContextMenu("Randomize")]
     public void Randomize()
     {
-        for (int i = 0; i < _items.Length; i++)
+        RandomizeLocations();
+        RandomizeRotations();
+    }
+
+    [ContextMenu("RandomizeLocations")]
+    private void RandomizeLocations()
+    {
+        List<Transform> randomItems = new List<Transform>(_itemsToSwitchLocations);
+        
+        for (int i = 0; i < _initialPositions.Length; i++)
         {
-            Transform itemTransform = _items[i];
-            bool itemActive = false;
+            Transform item = GetRandomItem(ref randomItems);
+            Vector3 position = item.localPosition;;
             if (Random.value > 0.5f)
             {
-                itemActive = true;
-                Vector3 position = Vector3.zero;
-
-                position.z = Random.Range(-_positionRange, _positionRange);
-                itemTransform.position = _transform.position + _initialPositions[i] + position;
+                position.z = _initialPositions[i] + Random.Range(-_positionRange, _positionRange);
             }
-            
-            itemTransform.gameObject.SetActive(itemActive);
+
+            item.localPosition = position;
+        }
+    }
+
+    [ContextMenu("RandomizeRotations")]
+    private void RandomizeRotations()
+    {
+        foreach (Transform item in _itemsToRotate)
+        {
+            float newAngle = Random.Range(-180.0f, 180.0f);
+            item.rotation = Quaternion.AngleAxis(newAngle, Vector3.up);
         }
     }
 
@@ -39,10 +57,21 @@ public class RandomTransformItems : MonoBehaviour
     {
         _transform = transform;
 
-        _initialPositions = new Vector3[_items.Length];
-        for (int i = 0; i < _items.Length; i++)
+        _initialPositions = new float[_itemsToSwitchLocations.Length];
+        for (int i = 0; i < _itemsToSwitchLocations.Length; i++)
         {
-            _initialPositions[i] = _items[i].localPosition;
+            _initialPositions[i] = _itemsToSwitchLocations[i].localPosition.z;
         }
+
+        Randomize();
+    }
+
+    private Transform GetRandomItem(ref List<Transform> items)
+    {
+        int index = Random.Range(0, items.Count);
+        Transform result = items[index];
+        items.RemoveAt(index);
+
+        return result;
     }
 }
